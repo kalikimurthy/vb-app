2bdbd77fb6513ce080acf652e47504453e7f06f7
saicanoe <saisamudr786@example.com>
2026-05-13 13:34:49 -0400

    Return full API pagination for demo schedule

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -87,7 +87,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 25,
+    "PAGE_SIZE": 100,
+    "PAGE_SIZE_QUERY_PARAM": "page_size",
+    "MAX_PAGE_SIZE": 500,
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.OrderingFilter",
